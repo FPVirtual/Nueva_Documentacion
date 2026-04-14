27def8f76729c22343fd81f63c6fb9bae6fc203e
Robert Sebastian Visu <robert.sebastian.visu@gmail.com>
2026-04-14 12:31:50 +0200

    feat: reemplazar comando docs:sync para usar un script que acepta la variable de entorno URL_REPO_DOCS

---
+++ scripts/sync-docs.js
@@ -0,0 +1,23 @@
+import cp from "node:child_process";
+import fs from "node:fs";
+import "dotenv/config";
+
+const repoUrl = process.env.URL_REPO_DOCS
+
+if(!repoUrl) {
+    console.error('Variable de entorno URL_REPO_DOCS no definida')
+    process.exit(1);
+}
+
+try {
+    console.log('Eliminando carpeta de documentación anterior...')
+    fs.rmSync('src/content/docs', { recursive: true, force: true })
+
+    console.log('Clonando repositorio de documentación...')
+    cp.execSync(`git clone ${repoUrl} src/content/docs`, { stdio: 'inherit' });
+
+    console.log('Documentación sincronizada correctamente')
+} catch (error) {
+    console.error('Error al sincronizar la documentación:', error);
+    process.exit(1);
+}
