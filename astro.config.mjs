067d5ff6e93fba7a96ff0c80210b17e51cd4ce97
Robert Sebastian Visu <robert.sebastian.visu@gmail.com>
2026-04-14 14:15:03 +0200

    feat: añadir url del link del icono del repo a partir de la variable de entorno URL_REPO_DOCS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
+import "dotenv/config";
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+
+// Env
+const repoUrl = process.env.URL_REPO_DOCS ?? ''
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [{ icon: 'github', label: 'GitHub', href: repoUrl }],
 		}),
 	],
 });
