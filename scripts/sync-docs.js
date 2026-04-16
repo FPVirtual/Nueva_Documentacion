c8b9da6f2be6101880a7561f75db2bd396dcae3c
Robert Sebastian Visu <robert.sebastian.visu@gmail.com>
2026-04-16 11:03:21 +0200

    feat: mensaje de error al sincronizar la documentacion más informativo

--- scripts/sync-docs.js
+++ scripts/sync-docs.js
@@ -2,22 +2,44 @@ import "dotenv/config";
 import cp from "node:child_process";
 import fs from "node:fs";
 
-const repoUrl = process.env.URL_REPO_DOCS
+const repoUrl = process.env.URL_REPO_DOCS;
 
-if(!repoUrl) {
-    console.error('Variable de entorno URL_REPO_DOCS no definida')
-    process.exit(1);
+if (!repoUrl) {
+  console.error("Variable de entorno URL_REPO_DOCS no definida");
+  process.exit(1);
 }
 
 try {
-    console.log('Eliminando carpeta de documentación anterior...')
-    fs.rmSync('src/content/docs', { recursive: true, force: true })
+  // Eliminar la carpeta de documentación anterior
+  console.log("Eliminando carpeta de documentación anterior...");
+  fs.rmSync("src/content/docs", { recursive: true, force: true });
 
-    console.log('Clonando repositorio de documentación...')
-    cp.execSync(`git clone ${repoUrl} src/content/docs`, { stdio: 'inherit' });
+  // Clonar el repositorio de documentación
+  console.log("Clonando repositorio de documentación...");
+  const resultado = cp.spawnSync(
+    "git",
+    ["clone", repoUrl, "src/content/docs"],
+    {
+      encoding: "utf-8",
+    },
+  );
+  // Error al ejecutar el comando Git
+  if (resultado.error) {
+    const mensaje = resultado.error.message;
+    throw new Error(`Fallo en el sistema al ejecutar Git -> ${mensaje}`);
+  }
+  // Error ejecutando Git
+  if (resultado.status !== 0) {
+    const mensaje = resultado.stderr || resultado.stdout || "Error desconocido";
 
-    console.log('Documentación sincronizada correctamente')
+    throw new Error(
+      `El proceso de clonado falló con codigo ${resultado.status}\n---\n${mensaje}---`,
+    );
+  }
+
+  // Proceso de sincronización exitoso
+  console.log("Documentación sincronizada correctamente");
 } catch (error) {
-    console.error('Error al sincronizar la documentación:', error);
-    process.exit(1);
+  console.error(`❌ Error al sincronizar la documentación:\n${error.message}`);
+  process.exit(1);
 }
